847e806e5b68db6b81595ee5759527831c646c9d
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-09-19 06:54:58 -0500

    Actually implement IntegerGameRule

--- src/main/java/dev/rndmorris/advgamerules/api/rules/IntegerGameRule.java
+++ src/main/java/dev/rndmorris/advgamerules/api/rules/IntegerGameRule.java
@@ -1,4 +1,51 @@
 package dev.rndmorris.advgamerules.api.rules;
 
-public class IntegerGameRule {
+import java.util.Collection;
+
+import net.minecraft.command.ICommandSender;
+import net.minecraft.nbt.NBTTagCompound;
+
+import dev.rndmorris.advgamerules.api.IGameRule;
+import dev.rndmorris.advgamerules.api.IRuleValue;
+import dev.rndmorris.advgamerules.api.values.IntegerValue;
+
+public class IntegerGameRule implements IGameRule {
+
+    private final String name;
+    private final int defaultValue;
+
+    public IntegerGameRule(String name, int defaultValue) {
+        this.name = name;
+        this.defaultValue = defaultValue;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public IRuleValue getDefaultValue() {
+        return new IntegerValue(defaultValue);
+    }
+
+    @Override
+    public IRuleValue readValueFromNBT(NBTTagCompound tag) {
+        if (!tag.hasKey(name)) {
+            return getDefaultValue();
+        }
+        return new IntegerValue(tag.getInteger(name));
+    }
+
+    @Override
+    public void writeValueToNBT(NBTTagCompound tag, IRuleValue value) {
+        if (value instanceof IntegerValue intValue) {
+            tag.setInteger(name, intValue.getValue());
+        }
+    }
+
+    @Override
+    public Collection<String> tabCompletionValues(ICommandSender commandSender) {
+        return null;
+    }
 }
